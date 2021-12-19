c50d1934a65c373cd8864e2d3d9003e4653df435
Anatoliy <bashkatov.spb@mail.ru>
2021-12-19 14:25:04 +0300

    Slider

--- script/script.js
+++ script/script.js
@@ -1,6 +1,22 @@
 const btn = document.getElementById('button');
 const menu = document.querySelector('.menu');
 const btnMenuOpen = document.querySelector('.menu__header--open');
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.2,
+    }
+  }
+
+});
 
 button.addEventListener('click', function() {
   menu.classList.add('open');
@@ -8,6 +24,7 @@ button.addEventListener('click', function() {
 });
 
 btnMenuOpen.addEventListener('click', function() {
-  menu.classList.remove('open');
+  menu.classList.remove('open');  
 
 });
+
